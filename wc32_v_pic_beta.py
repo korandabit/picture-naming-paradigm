99d5d28f58ce79ba450a806739e90241ff7fc189
Mark <mjkoranda@wisc.edu>
2018-10-04 09:06:06 -0500

    uploaded '1_pic_beta' main files
    development since orig piloting, done in early fall 2018

--- wc32_v_pic_beta.py
+++ wc32_v_pic_beta.py
@@ -49,18 +49,33 @@ the return function of get_keys from the hf.function seems to work. duplicate th
 # expand data file: look at other exp for things.
 # for whatever reason, itms 2-4 practice, don't register under 'if prac then'..
 
+instr1 = """Welcome and thank you for your participation.
+  
+For this experiment a series of images and words will briefly appear 
+on the screen one at a time. Please say say the word or name the picture quickly and accurately. After a brief period of time, the word or picture will disappear and a bell will sound. Do your best to beat the bell, but it is okay if you do not start before the bell sounds. Either way, say the first name you think of, at which point the experimenter will advance the trial and stop the bell.
 
-def presentStimuli(win,stimuli_list, exp, ID, dataPath,
-				   outputFolder = "/Data",
+Let the experimenter know when you're ready to practice. The experimenter will advance the trial as soon as you say STAIRS"""
+
+instr2 = """The plus sign ( + ) signals the beginning of the next picture or word. If you have not yet named the previous picture or word when the plus sign appears that is okay. However, do not try to do so at that time.
+
+Please let the experimenter know if you have any questions, or are ready to begin."""
+# def foo():
+	# friends = wint
+	# print(wint)
+# wint = 'tree'
+def presentStimuli(main_window,
+					dataPath,
+				    ID,
+				   stimuli_list,
 				   presTime = .1,
-				   catchBuffer = 0.5):
-				   
+				   catchBuffer = 0.5,
+				   outputFolder = "/Data"):
+	win = main_window
 	sceneVis = visual.ImageStim(win, image = None)
-
-	# for itemNum, cur_row in enumerate(stim_list[0:2]):
+	# foo()
+	
 	for itemNum, cur_row in enumerate(stimuli_list):
-		key_check = []
-		
+
 		participant = str(ID).zfill(2)
 		trial = str(cur_row[2]).zfill(3)
 		trial_type = cur_row[0]
@@ -78,8 +93,11 @@ def presentStimuli(win,stimuli_list, exp, ID, dataPath,
 
 		recorder = hf.AudioRecorder(audio_path)
 		
-		# some will be subbed out...
+		# vars to be recorded. some will be subbed out...
 		word, error, word_onset, word_duration = '','','',''
+
+		# set up key press watcher
+		key_check = []
 		cur_key = event.getKeys(keyList=['p','u','q','space'])
 		if cur_key:
 			key_check.append(cur_key[0])
@@ -90,34 +108,27 @@ def presentStimuli(win,stimuli_list, exp, ID, dataPath,
 		sceneVis.draw()
 		recorder.start()
 		
-		win.flip() # show stim
+		main_window.flip() # show stim
 
 		core.wait(presTime) # for this duration,
-		print("1 ",key_check)
-		win.flip() # then hide screen
-
-
+		# print("1 ",key_check)
+		main_window.flip() # then hide screen
 		
+		trial_duration = presTime
+		response=[]
 		if buzzerGo: # play the deadline buzzer
 			response,duration = hf.playAudio("bell.wav",catchBuffer)
+			trial_duration += duration
 			
 		recorder.stop()
-		print("2 ",key_check,response)
+		# print("2 ",key_check,response)
 		
-					
-		# write row
-		try:
-			trial_duration = duration + presTime
-		except:
-			trial_duration = presTime
 			
 		header = ["computer","datetime", "exp", "cbal", "participant", "trial", "trial_type", "img", "trial_duration", "audio_path", "audio_file", "word", "error", "word_onset", "word_duration"]
 		# this should be moved to outside the loop
 
-		cur_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+		# cur_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 		lrow = [computer, cur_date, exp, cbal, participant, trial, trial_type, img, trial_duration, audio_path, audio_file, word, error, word_onset, word_duration]
-		
-		# lrow = [cur_date,exp,participant,trial,trial_type,img,dur,audio_path,audio_file]
 		
 		# write long format.
 		try:
@@ -138,11 +149,11 @@ def presentStimuli(win,stimuli_list, exp, ID, dataPath,
 			
 		# write wide format.
 		if trial_type=="trgt":
-			intf = stimuli_list[itemNum - 1][0]
-			lrow[6] = intf
+			# lrow[6] = stimuli_list[itemNum - 1][0] # pull the label from previous trial to determine 'trial_type'
 			try:
 				hf.write_2('wdata.csv',lrow,header)
-				# print(lrow)
+				print("wdata")
+				print(lrow)
 			except:
 				print("error opening datafile")
 				hf.write_2('w{0}.csv'.format(file_date),lrow,header)
@@ -152,7 +163,7 @@ def presentStimuli(win,stimuli_list, exp, ID, dataPath,
 		
 		if pause_key in key_check or pause_key in response:
 			key_check = ''
-			hf.presentText(win, "*paused*", wait = True,
+			hf.presentText(main_window, "*paused*", wait = True,
 					timeDelay = 2, text_ht = 80)
 
 		if quit_key in key_check or quit_key in response:
@@ -160,73 +171,67 @@ def presentStimuli(win,stimuli_list, exp, ID, dataPath,
 			
 		if fixGo: # show the fix cross
 			fix_timer = core.Clock()
-			hf.presentText(win, "+", wait = False,
+			hf.presentText(main_window, "+", wait = False,
 					timeDelay = fixation_duration, text_ht = 80)
 					
 		event.getKeys(keyList=['p','u','q','space'])
 		
-instr1 = """Welcome and thank you for your participation.
-  
-For this experiment a series of images and words will briefly appear 
-on the screen one at a time. Please say say the word or name the picture quickly and accurately. After a brief period of time, the word or picture will disappear and a bell will sound. Do your best to beat the bell, but it is okay if you do not start before the bell sounds. Either way, say the first name you think of, at which point the experimenter will advance the trial and stop the bell.
-
-Let the experimenter know when you're ready to practice. The experimenter will advance the trial as soon as you say STAIRS"""
-
-instr2 = """The plus sign ( + ) signals the beginning of the next picture or word. If you have not yet named the previous picture or word when the plus sign appears that is okay. However, do not try to do so at that time.
-
-Please let the experimenter know if you have any questions, or are ready to begin."""
-	
-def runExp(stimuliDir, exp):
+def runExp():
 	# instructions = hf.getInstructions(instructionsDir)
+	main_window = visual.Window(size = [1400, 900], color = "white", units = 'pix')
 	ID, dataPath = hf.getID(exp)
-	print(dataPath)
-	win = visual.Window(size = [1400, 900], color = "white", units = 'pix')
 
 	# instructions and practice trial
-	hf.presentText(win, instr1, text_ht = 30)
-	presentStimuli(win,demo_item, exp, ID , dataPath, presTime = presTime, catchBuffer = 2.3) # practice trial
-	
+	hf.presentText(main_window, instr1, text_ht = 30)
+	presentStimuli(main_window, dataPath, ID, demo_item, presTime, catchBuffer = 2.3) # practice trial
+
 	# instructions + rest of trials.
-	hf.presentText(win, instr2, text_ht = 30)
-	presentStimuli(win,stim_list, exp, ID, dataPath, presTime = presTime, catchBuffer = 2.3)
+	hf.presentText(main_window, instr2, text_ht = 30)
+	presentStimuli(main_window, dataPath, ID, stimuli_list, presTime, catchBuffer = 2.3)
 
 if __name__ == '__main__':
-	instructionsDir = "/Instructions"
-	stimuliDir = "/stimuli/"
+	"""ALL HARDCODED DEPENDS:
+	lns: 199 (stimuliDir),204 (demo_item)
+	improve depends:
+	wdata should look at if 'target', but requires if 'trgt'
+	
+	"""
 
-
-	# import stimuli
-	demo_item = [['prac','prac01.gif','1',1330]]
-
-	with open('cbal_0.csv', 'rb') as f:
-		reader2=csv.reader(f)
-		stim_list = list(reader2)
-	stim_list = stim_list
-
+	## RUNTIME PARAMS
+	
+	#environment
 	computer = socket.gethostname().upper()
 	cur_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 	file_date = datetime.datetime.now().strftime("%Y%m%d_%H%M")
-	cbal = 'A1a'
 
-	## RUNTIME PARAMS
-	presTime  = 1.2 # how long before deadline buzzer sounds.
-	
-	# dev parameters
-	development_mode = True # if True, overwrites TEST data everytime.
-	if development_mode:
-		try:
-			shutil.rmtree("Data/wc32v.33_PTEST")
-			error = "deleted old dir(TEST)"
-		except:
-			print("TEST dir didn't exist.")
-			error = "failed: delete dir"
-	
+	# experiment
+	exp = "wc32v.33"
+		
+	# import stimuli
+	stimuliDir = "/stimuli/"
+	demo_item = [['prac','obj001accordion.jpg','1',1330]]
+
+	#import cbal
+	cbal = 'objects1.csv'
+	with open(cbal, 'rb') as f:
+		reader2=csv.reader(f)
+		stimuli_list = list(reader2)
+
+	#variables
 	buzzerGo = True # play buzzer slide
 	fixGo = True # play fixation slide
+
+	presTime  = 1.2 # how long before deadline buzzer sounds.
+
+	# control functions
 	quit_key = 'q'
 	pause_key = 'p'
-	
+
 	# runtime
-	runExp(stimuliDir = stimuliDir, exp = "wc32v.33")
-	
+	try:
+		shutil.rmtree("Data/wc32v.33_PTEST")
+	except:
+		print("TEST dir didn't exist.")
+
+	runExp()
 	
